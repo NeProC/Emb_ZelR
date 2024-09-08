be9ad24fae96b18564a1a3e1dd6c8e35186f3cc5
RZA <r-zelenev@mail.ru>
2024-09-08 18:59:45 +0300

    add task4.1, task4.2

---
+++ Lesson4/task4.2/largest3.c
@@ -0,0 +1,41 @@
+/*
+ * largest3.c
+ * 
+ * Copyright 2024 roman <roman@DESKTOP-RM33TSB>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int x, y, z;
+	scanf("%d %d %d", &x, &y, &z);
+	
+	if(x<y)
+	    x = y;
+	    
+	if(x<z)
+	    x = z;
+	    
+	printf("%d\n", x);        
+	return 0;
+}
+
