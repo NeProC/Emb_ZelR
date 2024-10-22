85c208ebe766ff0ef5e6d07ffbabe7aa68280cdd
Roman Zelenev <R.Zelenev@1440.space>
2024-10-22 18:42:27 +0300

    G1

--- Lesson10/G1.c
+++ Lesson10/G1.c
@@ -30,42 +30,46 @@
 
 int main(int argc, char **argv)
 {
-    char *input_f = "input.txt";
-    char *output_f = "output.txt";
-    char line_f[LINE];
-    char c;
+    char *input_f = "input.txt";					//Указатель на входной файл
+    char *output_f = "output.txt";					//Указатель на выходной файл
+    char line_f[LINE];								//Массив для записи содержимого 
+    char c;											//Переменная для считывания символа в входном файле
     
-    FILE *fp;
+    FILE *fp;										//Указатель на файл
     
-    if((fp = fopen(input_f, "r")) == NULL)
+    //~ if((fp = fopen(input_f, "r")) == NULL)
+    if((fp = fopen(input_f, "r")) == NULL)			//Открываем входной файл и проверяем что указатель не пустой
+    {
+        perror("Error");							//Вывод Error сообщения
+        //~ printf("Error occured while opening file \n");
+        return 1;
+    }
+    
+    int counter = 0;								//Счетчик количества символов
+    
+    while ((c = getc(fp)) != EOF && c != '\n')		//Посимвольно считываем charы в файле и проверяем
+    {
+        line_f[counter++] = c;						//Записываем считанное значение в массив
+    }
+    line_f[counter] = '\0';							//В конце записываем знак конца строки
+    
+    fclose(fp);										//Закрываем файл
+    
+    if((fp = fopen(output_f, "w")) == NULL)			//Открываем файл на запись
     {
         perror("Error");
         return 1;
     }
     
-    int counter = 0;
-    
-    while ((c = getchar()) != EOF && c != '\n')
+    for(int32_t i = 0; i < 3; i++)					//Вывод массива через цикл
     {
-        line_f[counter++];
-    }
-    line_f[counter] = '\n';
-    
-    fclose(fp);
-    
-    if((fp = fopen(output_f, "w")) == NULL)
-    {
-        perror("Error file");
-        return 1;
-    }
-    
-    for(int32_t i = 0; i < 3; i++)
-    {
+        if(i)										//Условие, чтобы не выводилась первая запятая
+            fprintf(fp, ", ");
         fprintf(fp, "%s", line_f);
-        fprintf(fp, ", ");
+        //~ fprintf(fp, ", ");
     }
-    fprintf(fp, " %d", counter);
-    fclose(fp);
+    fprintf(fp, " %d", counter);					//Вывод счетчика количества знаков
+    fclose(fp);										//Закрпываем файл вывода данных
     return 0;
 }
 
